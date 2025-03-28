ead413ba6e8c325b72f4f4291b905d9e31274640
0xnf <0xnf@winetech.com>
2025-03-28 14:24:02 +0900

    [server] adjust main.go to understand the new merged repo path structure

--- web/main.go
+++ web/main.go
@@ -40,17 +40,17 @@ type InventoryProg struct {
 }
 
 type EditItemRequest struct {
-	Name             string   `json:"name,omitempty"`
-	AcquiredDate     string   `json:"acquired_date,omitempty"`
-	PurchasePrice    float64  `json:"purchase_price,omitempty"`
-	PurchaseCurrency string   `json:"purchase_currency,omitempty"`
-	IsUsed           bool     `json:"is_used,omitempty"`
-	ReceivedFrom     string   `json:"received_from,omitempty"`
-	SerialNumber     string   `json:"serial_number,omitempty"`
+	Name              string  `json:"name,omitempty"`
+	AcquiredDate      string  `json:"acquired_date,omitempty"`
+	PurchasePrice     float64 `json:"purchase_price,omitempty"`
+	PurchaseCurrency  string  `json:"purchase_currency,omitempty"`
+	IsUsed            bool    `json:"is_used,omitempty"`
+	ReceivedFrom      string  `json:"received_from,omitempty"`
+	SerialNumber      string  `json:"serial_number,omitempty"`
 	PurchaseReference string  `json:"purchase_reference,omitempty"`
-	Notes            string   `json:"notes,omitempty"`
-	Extra            string   `json:"extra,omitempty"`
-	FuturePurchase   bool     `json:"future_purchase,omitempty"`
+	Notes             string  `json:"notes,omitempty"`
+	Extra             string  `json:"extra,omitempty"`
+	FuturePurchase    bool    `json:"future_purchase,omitempty"`
 }
 
 func (p *InventoryProg) list(limit, offset *uint32, sortBy string, orderBy string) ([]byte, error) {
@@ -92,7 +92,7 @@ var prog InventoryProg
 
 func main() {
 	prog = InventoryProg{
-		path: "..\\inventory_manager_rs\\target\\debug\\inventory_manager_rs.exe",
+		path: "..\\rs\\target\\debug\\inventory_manager_rs.exe",
 	}
 
 	// Serve static files from the static directory
